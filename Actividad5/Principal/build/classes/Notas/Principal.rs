a4f756fe98d2f15601fd10db8a5958f39cc33db4
Maylen Echavez Morales <maylen.em2018.2018@gmail.com>
2023-10-11 11:57:16 -0500

    cambios

---
+++ Actividad5/Principal/build/classes/Notas/Principal.rs
@@ -0,0 +1,3 @@
+notas.Notas
+notas.VentanaPrincipal
+notas.Principal
---
+++ Actividad6/ListaContactos/src/listacontactos/addFriend.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package listacontactos;
+
+/**
+ *
+ * @author maylen
+ */
+public class addFriend {
+    
+     public static void main(String[] args) {
+       Principal ventana = new Principal();
+       ventana.setVisible(true);
+    }
+    
+}
